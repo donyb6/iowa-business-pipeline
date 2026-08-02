568e13822378b989cc28f65fbc07d6289ca8270b
Nana Boako <obengdboako@gmail.com>
2026-08-02 11:47:19 +0000

    add update script for sliver layer

--- src/silver_clean_update.sql
+++ src/silver_clean_update.sql
@@ -1,5 +1,4 @@
 ALTER TABLE silver_active_iowa_business
-
 -- ra
 ADD COLUMN ra_address_1_altered BOOLEAN DEFAULT FALSE,
 ADD COLUMN ra_address_1_corrected VARCHAR(255),
@@ -14,8 +13,7 @@ ADD COLUMN ra_zip_altered BOOLEAN DEFAULT FALSE,
 ADD COLUMN ra_zip_corrected VARCHAR(20),
 
 ADD COLUMN ra_state_altered BOOLEAN DEFAULT FALSE,
-ADD COLUMN ra_state_corrected VARCHAR(2),
-
+ADD COLUMN ra_state_corrected VARCHAR(255),
 -- ho
 ADD COLUMN home_office_altered BOOLEAN DEFAULT FALSE,
 ADD COLUMN home_office_corrected VARCHAR(20),
@@ -33,7 +31,7 @@ ADD COLUMN ho_zip_altered BOOLEAN DEFAULT FALSE,
 ADD COLUMN ho_zip_corrected VARCHAR(20),
 
 ADD COLUMN ho_state_altered BOOLEAN DEFAULT FALSE,
-ADD COLUMN ho_state_corrected VARCHAR(2),
+ADD COLUMN ho_state_corrected VARCHAR(255),
 
 ADD COLUMN ho_country_altered BOOLEAN DEFAULT FALSE,
 ADD COLUMN ho_country_corrected VARCHAR(255);
