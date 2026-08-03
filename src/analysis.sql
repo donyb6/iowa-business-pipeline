d26dcc7cb999610559e81e6280b1b4a246bc041e
Nana Boako <obengdboako@gmail.com>
2026-08-03 23:19:19 +0000

    update analysis.sql

--- src/analysis.sql
+++ src/analysis.sql
@@ -7,6 +7,10 @@ FROM gold_dim_date;
 SELECT *
 FROM gold_dim_ho_location;
 
+SELECT *
+FROM gold_dim_ra_location;
 
+SELECT *
+FROM gold_fact_business;
 
--- view all tables
+-- join all tables as one
